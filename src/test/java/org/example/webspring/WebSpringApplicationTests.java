4e9130b774838caa4dacafd2fc985edd44c61913
tanjajustus <tbakhoment@gmail.com>
2025-02-19 01:59:33 -0800

    Spring:HelLoWorld

---
+++ src/test/java/org/example/webspring/WebSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.webspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
