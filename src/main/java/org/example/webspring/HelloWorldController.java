3f83994bb50d68da1055d82952914fc7a409b15f
tanjajustus <tbakhoment@gmail.com>
2025-02-19 02:08:09 -0800

    Spring:HelloWorld project

--- src/main/java/org/example/webspring/HelloWorldController.java
+++ src/main/java/org/example/webspring/HelloWorldController.java
@@ -1,6 +1,7 @@
 package org.example.webspring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,12 @@ public class HelloWorldController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, World!";
+       // http://localhost:8080/api/hello
     }
+    // Hier wird die Pfadvariable {name} verwendet
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name) {
+        return "Hallo, " + name + "!";
+    }
+    //http://localhost:8080/api/hello/Jon
 }
--- src/main/java/org/example/webspring/WebSpringApplication.java
+++ src/main/java/org/example/webspring/WebSpringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebSpringApplication.class, args);
+    }
 
 }
